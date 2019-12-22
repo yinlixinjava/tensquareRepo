04410a33ae20023c5ba7a60b1bc881d1f0b3488c
13269755565 <yinlixin123>
2019-12-22 13:52:31 +0800

    没有改动啥

--- tensquare_parent52/tensquare_rabbitmqtest/src/main/java/com/tensquare/rabbit/Customer1.java
+++ tensquare_parent52/tensquare_rabbitmqtest/src/main/java/com/tensquare/rabbit/Customer1.java
@@ -4,13 +4,14 @@ import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
-@RabbitListener(queues = "itcast")
+@RabbitListener(queues = "sms")
 public class Customer1 {
 
     @RabbitHandler
     public void getMsg(String msg){
         System.out.println("itcast："+msg);
     }
-
 }
